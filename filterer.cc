2b04b06256ccdc388d30886b4ba7199f47959c66
Timothy Gale <timothy.gale@cantab.net>
2012-05-29 21:57:07 +0100

    Added rescaling class

--- filterer.cc
+++ filterer.cc
@@ -538,8 +538,41 @@ Rescale::Rescale(cl::Context& context,
     const std::string sourceCode = 
     "__kernel void rescale(__read_only image2d_t input,"
                           "__write_only image2d_t output,"
-                          "const float scaleFactor)"
-    "{"
+                          "const float scaleFactor)\n"
+    "{\n"
+        "int x = get_global_id(0);\n"
+        "int y = get_global_id(1);\n"
+        
+        // Make sure there is work to be done before starting it
+        "if (x < get_image_width(output)"
+         "&& y < get_image_height(output)) {\n"
+
+            "sampler_t inputSampler ="
+                "CLK_NORMALIZED_COORDS_FALSE"
+                "| CLK_ADDRESS_MIRRORED_REPEAT"
+                "| CLK_FILTER_LINEAR;\n"
+
+
+            // The coordinates are scaled about the centre of the image,
+            // so find where the centre is
+            "int2 outCoords = (int2) (x, y);\n"
+
+            "float2 outCoordsRelCentre = (float2) (x, y) - "
+                "((float2) (get_image_width(output),"
+                           "get_image_height(output)) - 1.0f) / 2.0f;\n"
+
+
+            // Work out the input coordinates relative to its centre, then
+            // work back from there to find the (x,y) from the corner
+            "float2 inCoords = outCoordsRelCentre / scaleFactor"
+                "+ ((float2) (get_image_width(input),"
+                             "get_image_height(input)) - 1.0f) /  2.0f;\n"
+
+
+            // Read and write
+            "float val = read_imagef(input, inputSampler, inCoords).x;\n"
+            "write_imagef(output, outCoords, val);\n"
+        "}"
     "}";
 
     // Bundle the code up
@@ -563,6 +596,41 @@ Rescale::Rescale(cl::Context& context,
 }
 
 
+
+
+
+void Rescale::operator() 
+      (cl::CommandQueue& commandQueue,
+       const cl::Image2D& input,
+       cl::Image2D& output,
+       float scalingFactor,
+       const std::vector<cl::Event>& waitEvents,
+       cl::Event* doneEvent)
+{
+    const int wgSizeX_ = 16, wgSizeY_ = 16;
+
+    // Break the task up into blocks of 16 to ensure workgroups operate on
+    // pixels close together
+    cl::NDRange WorkgroupSize = {wgSizeX_, wgSizeY_};
+
+    // Make big enough to include all pixels (at least)
+    cl::NDRange GlobalSize = {
+        roundWGs(output.getImageInfo<CL_IMAGE_WIDTH>(), wgSizeX_), 
+        roundWGs(output.getImageInfo<CL_IMAGE_HEIGHT>(), wgSizeY_)
+    }; 
+
+    // Set all the arguments
+    kernel_.setArg(0, input);
+    kernel_.setArg(1, output);
+    kernel_.setArg(2, float(scalingFactor));
+
+
+    // Execute
+    commandQueue.enqueueNDRangeKernel(kernel_, cl::NullRange,
+                                      GlobalSize,
+                                      WorkgroupSize,
+                                      &waitEvents, doneEvent);
+}
 
 
 
---
+++ testRescale.cc
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+
+#define __CL_ENABLE_EXCEPTIONS
+#include "cl.hpp"
+
+#include "filterer.h"
+#include "clUtil.h"
+#include "dtcwt.h"
+#include <iomanip>
+
+#include <ctime>
+
+#include <stdexcept>
+
+#include <highgui.h>
+
+
+std::tuple<cl::Platform, std::vector<cl::Device>, 
+           cl::Context, cl::CommandQueue> 
+    initOpenCL();
+
+cl::Image2D createImage2D(cl::Context& context, cv::Mat& mat);
+
+
+void displayComplexImage(cl::CommandQueue& cq, cl::Image2D& image)
+{
+    const size_t width = image.getImageInfo<CL_IMAGE_WIDTH>(),
+                height = image.getImageInfo<CL_IMAGE_HEIGHT>();
+    float output[height][width][2];
+    readImage2D(cq, &output[0][0][0], image);
+
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x)
+            std::cout << output[y][x][0] 
+                      << "+i*" << output[y][x][1]<< "\t";
+
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+
+void displayRealImage(cl::CommandQueue& cq, cl::Image2D& image)
+{
+    const size_t width = image.getImageInfo<CL_IMAGE_WIDTH>(),
+                height = image.getImageInfo<CL_IMAGE_HEIGHT>();
+    float output[height][width];
+    readImage2D(cq, &output[0][0], image);
+
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x)
+            std::cout << output[y][x] << "\t"; 
+
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    try {
+
+        cl::Platform platform;
+        std::vector<cl::Device> devices;
+        cl::Context context;
+        cl::CommandQueue commandQueue; 
+        std::tie(platform, devices, context, commandQueue) = initOpenCL();
+
+        //-----------------------------------------------------------------
+        // Starting test code
+  
+        cv::Mat input = cv::Mat::zeros(32, 4, cv::DataType<float>::type);
+        input.at<float>(15,3) = 1.0f;
+        cl::Image2D inImage = createImage2D(context, input);
+        commandQueue.finish();
+
+        Rescale rescale = { context, devices };
+
+        cl::Image2D outImage
+            = createImage2D(context,
+                    2 * inImage.getImageInfo<CL_IMAGE_WIDTH>(),
+                    2 * inImage.getImageInfo<CL_IMAGE_HEIGHT>());
+
+        rescale(commandQueue, inImage, outImage, 2.0f);
+        commandQueue.finish();
+
+        displayRealImage(commandQueue, outImage);
+
+    }
+    catch (cl::Error err) {
+        std::cerr << "Error: " << err.what() << "(" << err.err() << ")"
+                  << std::endl;
+    }
+                     
+    return 0;
+}
+
+
+std::tuple<cl::Platform, std::vector<cl::Device>, 
+           cl::Context, cl::CommandQueue> 
+initOpenCL()
+{
+    // Get platform, devices, command queue
+
+    // Retrive platform information
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    if (platforms.size() == 0)
+        throw std::runtime_error("No platforms!");
+
+    std::vector<cl::Device> devices;
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+    // Create a context to work in 
+    cl::Context context(devices);
+
+    // Ready the command queue on the first device to hand
+    cl::CommandQueue commandQueue(context, devices[0]);
+
+    return std::make_tuple(platforms[0], devices, context, commandQueue);
+}
+
+
+cl::Image2D createImage2D(cl::Context& context, cv::Mat& mat)
+{
+    if (mat.type() == CV_32F) {
+        // If in the right format already, just create the image and point
+        // it to the data
+        return cl::Image2D(context, 
+                           CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+                           cl::ImageFormat(CL_LUMINANCE, CL_FLOAT), 
+                           mat.cols, mat.rows, 0,
+                           mat.ptr());
+    } else {
+        // We need to get it into the right format first.  Convert then
+        // send
+        cv::Mat floatedMat;
+        mat.convertTo(floatedMat, CV_32F);
+
+        return cl::Image2D(context, 
+                           CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+                           cl::ImageFormat(CL_LUMINANCE, CL_FLOAT), 
+                           floatedMat.cols, floatedMat.rows, 0,
+                           floatedMat.ptr());
+    }
+}
+
+
+
+
