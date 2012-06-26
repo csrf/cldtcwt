d0ae35fd4463cd0697ed991763266805b5428474
Timothy Gale <timothy.gale@cantab.net>
2012-06-26 23:26:56 +0100

    Initial image output
    Added abs kernel to calculate magnitude of the output at a given point

--- testOutput.cc
+++ testOutput.cc
@@ -30,6 +30,105 @@ std::tuple<cl::Platform, std::vector<cl::Device>,
            cl::Context, cl::CommandQueue> 
     initOpenCL();
 
+
+class Abs {
+    // Kernel that takes a two-component (i.e. complex) image, and puts out a
+    // single-component (magnitude) image
+
+public:
+
+    Abs() = default;
+    Abs(const Abs&) = default;
+    Abs(cl::Context& context, const std::vector<cl::Device>& devices);
+
+    void operator() (cl::CommandQueue& cq, const cl::Image2D& input,
+                                           const cl::Image2D& output,
+                     const std::vector<cl::Event>& waitEvents
+                        = std::vector<cl::Event>(),
+                     cl::Event* doneEvent = nullptr);
+
+private:
+
+    cl::Context context_;
+    cl::Kernel kernel_;
+
+};
+
+
+#define STRING(t) #t
+
+Abs::Abs(cl::Context& context, const std::vector<cl::Device>& devices)
+{
+    std::string src = STRING(
+        __kernel void absKernel(__read_only image2d_t input,
+                          __write_only image2d_t output)
+        {
+            sampler_t s = CLK_NORMALIZED_COORDS_FALSE | CLK_FILTER_NEAREST;
+
+            int x = get_global_id(0);
+            int y = get_global_id(1);
+
+            if (x < get_image_width(output)
+                && y < get_image_height(output)) {
+
+                float2 valIn = read_imagef(input, s, (int2)(x, y)).xy;
+                write_imagef(output, (int2)(x, y), fast_length(valIn));
+            }
+
+        }
+    );
+
+    // Bundle the code up
+    cl::Program::Sources source;
+    source.push_back(std::make_pair(src.c_str(), src.length()));
+
+    // Compile it...
+    cl::Program program(context, source);
+    try {
+        program.build(devices);
+    } catch(cl::Error err) {
+	    std::cerr 
+		    << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0])
+		    << std::endl;
+	    throw;
+    } 
+        
+    // ...and extract the useful part, viz the kernel
+    kernel_ = cl::Kernel(program, "absKernel");
+}
+
+
+
+void Abs::operator() (cl::CommandQueue& cq, const cl::Image2D& input,
+                                       const cl::Image2D& output,
+                 const std::vector<cl::Event>& waitEvents,
+                 cl::Event* doneEvent)
+{
+    const int wgSize = 16;
+
+    cl::NDRange workgroupSize = {wgSize, wgSize};
+
+    cl::NDRange globalSize = {
+        roundWGs(output.getImageInfo<CL_IMAGE_WIDTH>(), wgSize), 
+        roundWGs(output.getImageInfo<CL_IMAGE_HEIGHT>(), wgSize)
+    }; 
+
+
+    // Set all the arguments
+    kernel_.setArg(0, input);
+    kernel_.setArg(1, output);
+
+    // Execute
+    cq.enqueueNDRangeKernel(kernel_, cl::NullRange,
+                            globalSize, workgroupSize,
+                            &waitEvents, doneEvent);
+}
+
+
+
+
+
+
 class Main {
 private:
     Dtcwt dtcwt;
@@ -44,6 +143,11 @@ private:
     cl::CommandQueue commandQueue; 
 
     cl::Image2D inImage;
+
+    // For interop OpenGL/OpenCL
+    cl::Image2DGL dispImage;
+
+    Abs abs;
 
 public:
 
@@ -85,6 +189,13 @@ Main::Main()
 
         std::tie(platform, devices, context, commandQueue) = initOpenCL();
 
+        abs = Abs(context, devices);
+
+        dispImage = cl::Image2DGL(context, CL_MEM_READ_WRITE,
+                                  GL_TEXTURE_2D, 0,
+                                  texture);
+
+
         const int numLevels = 6;
         const int startLevel = 1;
 
@@ -108,6 +219,7 @@ Main::Main()
         std::cout << "Creating the subband output images..." << std::endl;
         out = DtcwtOutput(env);
 
+        dtcwt(commandQueue, inImage, env, out);
 
 
     }
@@ -139,6 +251,16 @@ bool Main::update(void)
 
     app.SetActive();
 
+    // Synchronise OpenGL
+    glFinish();
+
+    // Synchronise OpenCL
+    std::vector<cl::Memory> mems = {dispImage};
+    commandQueue.enqueueAcquireGLObjects(&mems);
+
+    abs(commandQueue, out.subbands[0].sb[0], dispImage);
+
+    commandQueue.enqueueReleaseGLObjects(&mems);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
