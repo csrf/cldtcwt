f76549db34063b8f3da0bd5b7c6ce5e6cbc360e6
Timothy Gale <timothy.gale@cantab.net>
2012-07-10 21:41:50 +0100

    Start work on the descriptor extraction test bed

---
+++ testDescriptor.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+
+#define __CL_ENABLE_EXCEPTIONS
+#include "cl.hpp"
+
+#include "filterer.h"
+#include "clUtil.h"
+#include "dtcwt.h"
+#include <iomanip>
+
+#include <sys/timeb.h>
+
+#include <stdexcept>
+
+#include <highgui.h>
+#include "extractDescriptors.h"
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        std::cerr << "Provide an image filename" << std::endl;
+        return -1;
+    }
+
+    try {
+
+        const int numLevels = 6;
+        const int startLevel = 1;
+
+        CLContext context;
+
+        // Ready the command queue on the first device to hand
+        cl::CommandQueue cq(context.context, context.devices[0]);
+        Dtcwt dtcwt(context.context, context.devices, cq);
+
+        // Ready the keypoint extractor
+        DescriptorExtracter(context.context, context.devices);
+
+        // Read in image
+        cv::Mat bmp = cv::imread(argv[1], 0);
+        cl::Image2D inImage = createImage2D(context.context, bmp);
+
+        // Create temporaries and outputs for the DTCWT
+        DtcwtTemps env = dtcwt.createContext(bmp.cols, bmp.rows,
+                                           numLevels, startLevel);
+        DtcwtOutput out(env);
+
+        // Perform the transform
+        dtcwt(cq, inImage, env, out);
+
+        // Extract descriptors
+
+    }
+    catch (cl::Error err) {
+        std::cerr << "Error: " << err.what() << "(" << err.err() << ")"
+                  << std::endl;
+    }
+                     
+    return 0;
+}
+
+
