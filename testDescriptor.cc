ad641bee7e7773bed1861adb9bee9f6d2f9289e2
Timothy Gale <timothy.gale@cantab.net>
2012-07-17 21:38:41 +0100

    Switched over to testing the full descriptor extracter

--- testDescriptor.cc
+++ testDescriptor.cc
@@ -38,25 +38,9 @@ int main(int argc, char** argv)
         cl::CommandQueue cq(context.context, context.devices[0]);
         Dtcwt dtcwt(context.context, context.devices, cq);
 
-        const float pi = 4 * atan(1);
-
-        // Pattern of locations to sample at
-        // Set up the centre
-        std::vector<Coord> pattern = {{0 ,0}};
-
-        // Set up the circle
-        for (int n = 0; n < 12; ++n) {
-            pattern.push_back({float(sin(float(n) / 12.f * 2.f * pi)),
-                               float(cos(float(n) / 12.f * 2.f * pi))});
-        }
-
-
-        // Ready the keypoint extractor
-        Interpolator
-            descriptorExtracter(context.context, context.devices, cq,
-                                pattern, 1.f,
-                                13, 0,
-                                2);
+        DescriptorExtracter describer(context.context,
+                                      context.devices,
+                                      cq);
 
         // Read in image
         cv::Mat bmp = cv::imread(argv[1], 0) / 255.0f;
@@ -72,14 +56,14 @@ int main(int argc, char** argv)
                                          {14.f, 14.5f});
 
         cl::Buffer output = createBuffer(context.context, cq, 
-                                 std::vector<float>(pattern.size() * 12));
+                                 std::vector<float>(14 * 12));
 
         // Perform the transform
         dtcwt(cq, inImage, env, out);
 
         // Extract descriptors
-        descriptorExtracter(cq, out.subbands[0], kplocs, 2,
-                            output);
+        describer(cq, out.subbands[0], out.subbands[1], kplocs, 1,
+                      output);
         
         // Read them out
         saveComplexBuffer("interpolations.dat", cq, output);
