afbf362d76d38d92b35f1be5a676464ff96e2aa0
Timothy Gale <timothy.gale@cantab.net>
2012-12-20 10:37:44 +0000

    Start DecimateFilterX from FilterX

---
+++ src/BufferTrials/DecimateFilterX/decimateFilterX.h
@@ -0,0 +1,48 @@
+#ifndef FILTERX_H
+#define FILTERX_H
+
+
+#ifndef __CL_ENABLE_EXCEPTIONS
+#define __CL_ENABLE_EXCEPTIONS
+#endif
+#include "CL/cl.hpp"
+
+
+#include "BufferTrials/imageBuffer.h"
+
+
+class FilterX {
+    // Straightforward convolution along the x axis, with an odd-
+    // lengthed set of coefficients.  The images are padded.
+
+public:
+
+    FilterX() = default;
+    FilterX(const FilterX&) = default;
+    FilterX(cl::Context& context, 
+            const std::vector<cl::Device>& devices,
+            std::vector<float> filter);
+
+    void operator() (cl::CommandQueue& cq, ImageBuffer& input,
+                                           ImageBuffer& output,
+                     const std::vector<cl::Event>& waitEvents
+                        = std::vector<cl::Event>(),
+                     cl::Event* doneEvent = nullptr);
+
+private:
+
+    cl::Context context_;
+    cl::Kernel kernel_;
+    cl::Buffer filter_;
+
+    size_t filterLength_;
+
+    static const size_t padding_ = 8,
+                        workgroupSize_ = 16;
+
+};
+
+
+
+#endif
+
