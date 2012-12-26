761cb54ed8c33d21b6835df40730cf1cd4cbea18
Timothy Gale <timothy.gale@cantab.net>
2012-12-26 21:02:47 +0000

    Activate output tree swapping in DecimateFilterX

--- src/BufferTrials/DecimateFilterX/decimateFilterX.cc
+++ src/BufferTrials/DecimateFilterX/decimateFilterX.cc
@@ -25,7 +25,10 @@ DecimateFilterX::DecimateFilterX(cl::Context& context,
     compilerOptions << "-D WG_W=" << workgroupSize_ << " "
                     << "-D WG_H=" << workgroupSize_ << " "
                     << "-D FILTER_LENGTH=" << filter.size() << " "
-                    << "-D PADDING=" << padding_;
+                    << "-D PADDING=" << padding_ << " ";
+
+    if (swapOutputPair)
+        compilerOptions << "-D SWAP_TREE_OUTPUTS ";
 
     // Compile it...
     cl::Program program(context, source);
