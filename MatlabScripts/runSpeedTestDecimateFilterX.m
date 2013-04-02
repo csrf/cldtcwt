dc69f34dc03a083e0c8837070e3234540c385071
Timothy Gale <teg28@cam.ac.uk>
2013-04-02 11:40:27 +0100

    Add timing scripts

--- MatlabScripts/runSpeedTestDecimateFilterX.m
+++ MatlabScripts/runSpeedTestDecimateFilterX.m
@@ -1,14 +1,23 @@
-function [dt] = runSpeedTestDecimateFilterX(dims, filterLength, pad)
-% [dt] = runSpeedTestDecimateFilterX(dims, filterLength, pad)
+function [dt] = runSpeedTestDecimateFilterX(dims, filterLength, pad, suffix)
+% [dt] = runSpeedTestDecimateFilterX(dims, filterLength, pad[, suffix])
 % 
 % Runs clDTCWT-SpeedTest-DecimateFilterX, and returns the results (in ms) as
 % numbers.  dims is a two-element vector containing the size of the image.
 % filterLength is the number of taps to use in the filter; pad is whether to 
 % run the symmetric extension kernel before each call to the filtering kernel.
 % Runs each kernel 10,000 times.
+%
+% If suffix is specified, it is appended with '-alt-' suffix to the name of the 
+% programme called. 
+
+    if ~exist('suffix', 'var')
+        suffix = '';
+    else
+        suffix = ['-Alt-' suffix];
+    end
 
     % Call with dimensions, filter length and number of runs to test over
-    [~, t] = system(['clDTCWT-SpeedTest-DecimateFilterX ' ...
+    [~, t] = system(['clDTCWT-SpeedTest-DecimateFilterX' suffix ' ' ...
                      num2str(dims(1)) ' ' num2str(dims(2)) ' '...
                      num2str(filterLength) ' 10000 '...
                      num2str(pad)]);
