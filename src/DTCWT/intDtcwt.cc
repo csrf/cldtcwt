7ccdf39670241c98a7f1f3353e7462883b4462ea
Timothy Gale <teg28@cam.ac.uk>
2013-04-08 16:02:23 +0100

    Started adding interleaved DTCWTs

---
+++ src/DTCWT/intDtcwt.cc
@@ -0,0 +1,163 @@
+// Implementation of interleaved DTCWT
+
+#include "intDtcwt.h"
+
+
+IntDtcwt::IntDtcwt(cl::Context& context,
+
+                   std::vector<cl::Device>& devices,
+                   float scaleFactor)
+    : context_{context},
+      scaleImageToImageBuffer_{context, devices},
+      dtcwt_{context, devices, scaleFactor}
+{}
+
+
+
+void IntDtcwt::operator() (cl::CommandQueue& cq,
+                           cl::Image2D& image,
+                           IntDtcwtOutput& output,
+                           const std::vector<cl::Event>& waitEvents)
+{
+    for (size_t n = 0; n < output.scales_.size(); ++n) {
+
+        cl::Event imageBufferReady;
+        scaleImageToImageBuffer_(cq, 
+                                 image, output.inputImages_[n], 
+                                 output.scales_[n],
+                                 waitEvents, &imageBufferReady);
+        
+        dtcwt_(cq, output.inputImages_[n], output.dtcwtTemps_[n], 
+                   output.dtcwtOutputs_[n], 
+                   {imageBufferReady});
+
+    }
+
+}
+
+
+
+IntDtcwtOutput IntDtcwt::createOutputs(size_t width, size_t height, 
+                                       size_t startLevel, size_t numLevels,
+                                       const std::vector<float> &scales)
+{
+    IntDtcwtOutput output;
+
+    // Straightforward copy of some parameters
+    output.inputWidth_ = width;
+    output.inputHeight_ = height;
+    output.startLevel_ = startLevel;
+    output.numLevels_ = numLevels;
+    output.scales_ = scales;
+
+    for (float s: output.scales_) {
+
+        // Generate an ImageBuffer to scale into
+        size_t w = s * width, h = s * height;
+
+        // Create a place for the scaled image
+        output.inputImages_.emplace_back(context_, CL_MEM_READ_WRITE,
+                                         w, h, 16, 32);
+
+        // Create the temporary for this tree
+        output.dtcwtTemps_.emplace_back(context_, w, h,
+                                        startLevel, numLevels);
+
+        // Create the outputs
+        output.dtcwtOutputs_
+            .push_back(output.dtcwtTemps_.back().createOutputs());
+
+    }
+
+    return output;
+}
+
+
+
+
+
+// Get the number of trees
+size_t IntDtcwtOutput::numTrees() const
+{
+    return scales_.size();
+}
+
+// Get the number of levels calculated per tree
+size_t IntDtcwtOutput::numLevels() const
+{
+    return numLevels_;
+}
+
+// Get the number of the first level calculated
+// in all the trees
+size_t IntDtcwtOutput::startLevel() const
+{
+    return startLevel_;
+}
+
+// Get the index of the output
+size_t IntDtcwtOutput::idxFromTreeLevel(size_t tree, size_t level) const
+{
+    return tree + numTrees() * (level - startLevel_);
+}
+
+// Convert from index to level and tree number
+std::tuple<size_t,size_t> IntDtcwtOutput::treeLevelFromIdx(size_t idx) const
+{
+    size_t tree = idx % numTrees();
+    size_t level = startLevel_ + (idx - tree) / numTrees();
+    return std::make_tuple(tree, level); 
+}
+
+// Get the scale of the level number or index
+float IntDtcwtOutput::scale(size_t tree, size_t level) const
+{
+    return scales_[tree] * (1 << level);
+}
+
+float IntDtcwtOutput::scale(size_t idx) const
+{
+    size_t tree, level;
+    std::tie(tree, level) = treeLevelFromIdx(idx);
+    return scale(tree, level);
+}
+
+
+// Get requested subbands by tree and tree level
+Subbands& IntDtcwtOutput::level(size_t tree, size_t level)
+{
+    return dtcwtOutputs_[tree].level(level);
+}
+
+
+const Subbands& IntDtcwtOutput::level(size_t tree, size_t level) const
+{
+    return dtcwtOutputs_[tree].level(level);
+}
+
+
+// Get the subband by index size_to all the trees
+Subbands& IntDtcwtOutput::operator[](size_t idx)
+{
+    size_t tree, level;
+    std::tie(tree, level) = treeLevelFromIdx(idx);
+    return dtcwtOutputs_[tree].level(level);
+}
+
+const Subbands& IntDtcwtOutput::operator[](size_t idx) const
+{
+    size_t tree, level;
+    std::tie(tree, level) = treeLevelFromIdx(idx);
+    return dtcwtOutputs_[tree].level(level);
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/DTCWT/intDtcwt.h
@@ -0,0 +1,109 @@
+#ifndef INT_DTCWT_H
+#define INT_DTCWT_H
+
+// Interleaved DTCWT implementation.  Uses the single tree DTCWT.
+
+#include "dtcwt.h"
+#include "Filter/imageBuffer.h"
+#include "Filter/ScaleImageToImageBuffer/scaleImageToImageBuffer.h"
+
+#include <tuple>
+#include <vector>
+
+
+// Forward declaration
+class IntDtcwtOutput;
+
+class IntDtcwt {
+
+    // Class to perform an interleaved DTCWT operation
+
+public:
+    IntDtcwt() = default;
+    IntDtcwt(const IntDtcwt&) = default;
+    // Allow default and copy construction.  Nothing special for these,
+    // so just allow it.
+
+    IntDtcwt(cl::Context& context,
+             std::vector<cl::Device>& devices,
+             float scaleFactor = 1.f);
+    // Scale factor specifies a constant to multiply by at each level.
+    // Consider using 0.5 to keep magnitudes reasonably similar between
+    // levels.
+    
+    void operator() (cl::CommandQueue& commandQueue,
+                     cl::Image2D& image,
+                     IntDtcwtOutput& output,
+                     const std::vector<cl::Event>& waitEvents = {});
+    // Analyse image into output, using the specified command queue after
+    // waitEvents (if any) have completed.
+    
+    IntDtcwtOutput createOutputs(size_t width, size_t height, 
+                                 size_t startLevel, size_t numLevels,
+                                 const std::vector<float> &scales);
+    // Create the outputs (including temporary variables)
+                  
+
+private:
+
+    cl::Context context_;
+    ScaleImageToImageBuffer scaleImageToImageBuffer_;
+    Dtcwt dtcwt_;
+
+};
+
+
+
+
+class IntDtcwtOutput {
+
+    // Class to hold the results of an interleaved DTCWT operation
+
+    friend class IntDtcwt;
+    // Allow IntDtcwt to manipulate the output behind the scenes
+
+public:
+
+    // Get the number of trees
+    size_t numTrees() const;
+
+    // Get the number of levels calculated per tree
+    size_t numLevels() const;
+
+    // Get the number of the first level calculated
+    // in all the trees
+    size_t startLevel() const;
+
+    // Get the index of the output
+    size_t idxFromTreeLevel(size_t tree, size_t level) const;
+
+    // Convert from index to level and tree number
+    std::tuple<size_t,size_t> treeLevelFromIdx(size_t idx) const;
+
+    // Get the scale of the level number or index
+    float scale(size_t tree, size_t level) const;
+    float scale(size_t idx) const;
+
+    // Get requested subbands by tree and tree level
+    Subbands& level(size_t tree, size_t level);
+    const Subbands& level(size_t tree, size_t level) const;
+
+    // Get the subband by index size_to all the trees
+    Subbands& operator[](size_t idx);
+    const Subbands& operator[](size_t idx) const;
+
+private:
+
+    float inputWidth_, inputHeight_;
+    size_t startLevel_, numLevels_;
+
+    std::vector<float> scales_;
+
+    std::vector<ImageBuffer<cl_float>> inputImages_;
+    std::vector<DtcwtTemps> dtcwtTemps_;
+    std::vector<DtcwtOutput> dtcwtOutputs_;
+
+};
+
+#endif
+
