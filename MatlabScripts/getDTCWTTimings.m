c202d0a601fc03cb1f0f3b5b3a8106ac6ed550db
Timothy Gale <teg28@cam.ac.uk>
2013-04-16 16:26:52 +0100

    Added script for DTCWT timing

---
+++ MatlabScripts/getDTCWTTimings.m
@@ -0,0 +1,26 @@
+% Time how long it takes to run DTCWT.  All times are in ms.  Saves
+% results to filterTimings.mat.
+
+numLevels = 1:10;
+
+% Vary the number of filter taps
+tNumLevels = nan(length(numLevels),1);
+for n = 1:length(numLevels)
+    tNumLevels(n) = runSpeedTestDTCWT([1280 720], numLevels(n));
+end
+
+imageSizes = int32(1.5.^(-10:0.5:1)' * [1280 720] / 4) * 4;
+
+% Vary the size of the image
+tSizes = nan(size(imageSizes, 1),1);
+for n = 1:size(tSizes, 1)
+    tSizes(n) = runSpeedTestDTCWT(imageSizes(n,:), 6);
+end
+
+save('dtcwtTimings.mat', ...
+     'numLevels', 'tNumLevels',...
+     'imageSizes', 'tSizes');
+
+
+
+
