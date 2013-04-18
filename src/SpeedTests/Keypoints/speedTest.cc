f0260520344b57464808485325b3e5e369ee0a88
Timothy Gale <teg28@cam.ac.uk>
2013-04-18 15:58:29 +0100

    Make speed test for keypoints compile and run

--- src/SpeedTests/Keypoints/speedTest.cc
+++ src/SpeedTests/Keypoints/speedTest.cc
@@ -8,6 +8,11 @@
 
 #include <chrono>
 
+
+#include <ImfInputFile.h>
+#include <ImfFrameBuffer.h>
+#include <half.h>
+
 std::tuple<cl::Platform, std::vector<cl::Device>, cl::Context> 
     initOpenCL();
 
@@ -15,45 +20,121 @@ typedef std::chrono::duration<double, std::milli>
     DurationMilliseconds;
 
 
+struct Calculator {
 
-int main()
-{
-    const int width = 1280, height = 720;
-    const int numLevels = 3;
-    const int startLevel = 2;
-    const size_t maxNumKeypoints = 1000;
+    Dtcwt dtcwt;
+
+    InterpMapEigen energyMap;
+    PeakDetector peakDetector;
+
+    DescriptorExtracter descriptorExtracter;
+
+    Calculator(cl::Context context,
+               std::vector<cl::Device> devices);
+
+};
 
 
+Calculator::Calculator(cl::Context context,
+                       std::vector<cl::Device> devices)
+ :
+    dtcwt {context, devices, 0.5},
 
-    cl::Platform platform;
-    std::vector<cl::Device> devices;
-    cl::Context context;
+    energyMap {context, devices},
+    peakDetector {context, devices},
 
-    std::tie(platform, devices, context) = initOpenCL();
-    cl::Device device {devices[0]};
-
-    cl::CommandQueue commandQueue {context, device}; 
-
-    Dtcwt dtcwt {context, {device}, 0.5};
-    InterpMapEigen energyMap {context, {device}};
-    PeakDetector peakDetector {context, {device}};
-
-    DescriptorExtracter descriptorExtracter {
-        context, {device},
+    descriptorExtracter {
+        context, devices,
         peakDetector.getPosLength()
-    };
+    }
+
+{}
 
 
-    DtcwtTemps dtcwtTemps {context, width, height, startLevel, numLevels};;
-    DtcwtOutput dtcwtOut {dtcwtTemps.createOutputs()};
+std::vector<float> readY(const char filename[], 
+                        size_t& width, size_t& height)
+{
+    Imf::InputFile file(filename);
 
-    std::vector<cl::Image2D> energyMaps;
-    std::vector<cl::Event> energyMapsDone;
+    Imath::Box2i window = file.header().dataWindow();
+    width = window.max.x - window.min.x + 1;
+    height = window.max.y - window.min.y + 1;
+
+    Imf::FrameBuffer frameBuffer;
+
+    std::vector<float> values(width * height);
+
+    frameBuffer.insert("Y", 
+        Imf::Slice {
+            Imf::FLOAT, 
+            reinterpret_cast<char*>
+                (&values[0] - window.min.x - window.min.y * width),
+            sizeof(half), sizeof(half) * width,
+            1, 1, 0.f
+        });
+                                  
+    file.setFrameBuffer(frameBuffer);
+    file.readPixels(window.min.y, window.max.y);
+
+    return values;
+}
+
+
+struct Workings {
+
+    DtcwtTemps dtcwtTemps;
+    DtcwtOutput dtcwtOut;
 
     std::vector<float> scales; // List of the scale of each energy map, i.e. 
                                // how many pixels in the original image each
                                // pixel in the new image represents
-                               
+
+    std::vector<cl::Image2D> energyMaps;
+    std::vector<cl::Event> energyMapsDone;
+    std::vector<cl::Image*> emPointers;
+
+    PeakDetectorResults peakDetectorResults;
+
+    cl::Buffer descriptors;
+    std::vector<cl::Event> descriptorsDone;
+
+    Workings(cl::Context& context, 
+             size_t width, size_t height, 
+             size_t startLevel, size_t numLevels,
+             PeakDetector& peakDetector, 
+             size_t maxNumKeypoints, 
+             size_t descriptorSize);
+
+};
+
+
+
+
+Workings::Workings(cl::Context& context, 
+                   size_t width, size_t height, 
+                   size_t startLevel, size_t numLevels,
+                   PeakDetector& peakDetector, 
+                   size_t maxNumKeypoints, 
+                   size_t descriptorSize)
+    :
+    dtcwtTemps {context, width, height, startLevel, numLevels},
+    dtcwtOut {dtcwtTemps.createOutputs()},
+
+    peakDetectorResults {
+       peakDetector.createResultsStructure(
+                std::vector<size_t>(numLevels - 1, maxNumKeypoints),
+                maxNumKeypoints
+            )
+    },
+
+    descriptors {
+        context, CL_MEM_READ_WRITE,
+        maxNumKeypoints * descriptorSize
+    },
+
+    descriptorsDone { 2*(numLevels-1) }
+{
+    
     // Create energy maps for each output level (other than the last,
     // which is only there for coarse detections)
     float s = 2;
@@ -70,61 +151,97 @@ int main()
         scales.push_back(s *= 2);
     }
 
-    PeakDetectorResults peakDetectorResults 
-      = peakDetector.createResultsStructure(
-                std::vector<size_t>(energyMaps.size(), maxNumKeypoints),
-                maxNumKeypoints
-            );
- 
-    // Output of descriptors
-    cl::Buffer descriptors {
-        context, CL_MEM_READ_WRITE,
-        maxNumKeypoints * descriptorExtracter.getNumFloatsInDescriptor()
-            * sizeof(cl_uint),
-    };
-
-    std::vector<cl::Event> descriptorsDone_;
-
-
-
- 
-
-    // Transform
-    //dtcwt(commandQueue, input, dtcwtTemps, dtcwtOut, {}/* wait events */);
-
-    // Calculate energy
-    for (int l = 0; l < energyMaps.size(); ++l)
-        energyMap(commandQueue, 
-                  dtcwtOut.level(dtcwtOut.startLevel() + l), 
-                  energyMaps[l], 
-                  dtcwtOut.doneEvents(dtcwtOut.startLevel() + l), 
-                  &energyMapsDone[l]);
-
     // Adapt to input format of peakDetector, which takes a list of pointers
-    std::vector<cl::Image*> emPointers;
     for (auto& e: energyMaps)
         emPointers.push_back(&e);
+}
+
+
+
+
+
+
+int main()
+{
+    const int numLevels = 3;
+    const int startLevel = 2;
+    const size_t maxNumKeypoints = 1000;
+
+
+    size_t width, height;
+    std::vector<float> values = readY("logo.exr", width, height);
+
+
+    cl::Platform platform;
+    std::vector<cl::Device> devices;
+    cl::Context context;
+
+    std::tie(platform, devices, context) = initOpenCL();
+    cl::Device device {devices[0]};
+
+    cl::CommandQueue commandQueue {context, device}; 
+
+    Calculator calculator {context, {device}};
+                              
+    Workings workings {
+        context, 
+        width, height, 
+        startLevel, numLevels,
+        calculator.peakDetector,
+        maxNumKeypoints,
+        calculator.descriptorExtracter.getNumFloatsInDescriptor()
+             * sizeof(cl_float)
+    };
+
+    std::cout << width << "x" << height << std::endl;
+
+    ImageBuffer<float> input {
+        context, CL_MEM_READ_WRITE, 
+        width, height,
+        16, 32
+    };
+
+    cl::Event inputReady;
+    input.write(commandQueue, &values[0], {}, &inputReady);
+ 
+    // Transform
+    calculator.dtcwt(commandQueue, input, workings.dtcwtTemps, workings.dtcwtOut, {inputReady});
+
+
+
+
+
+    // Calculate energy
+    for (int l = 0; l < workings.energyMaps.size(); ++l) 
+        calculator.energyMap(commandQueue, 
+                  workings.dtcwtOut.level(workings.dtcwtOut.startLevel() + l), 
+                  workings.energyMaps[l], 
+                  workings.dtcwtOut.doneEvents(workings.dtcwtOut.startLevel() + l), 
+                  &workings.energyMapsDone[l]);
 
     // Look for peaks
-    peakDetector(commandQueue, emPointers, scales, 0.02, 0.f,
-                               peakDetectorResults,
-                               energyMapsDone);
+    calculator.peakDetector(commandQueue, workings.emPointers, workings.scales, 0.02, 0.f,
+                               workings.peakDetectorResults,
+                               workings.energyMapsDone);
+
+
+
 
     // Extract the descriptors
-    for (size_t l = 0; l < (energyMaps.size() - 1); ++l) {
-        descriptorExtracter(commandQueue, 
-                dtcwtOut[l], scales[l],      // Subband
-                dtcwtOut[l+1], scales[l+1],  // Parent subband
-                peakDetectorResults.list(),         // Locations of keypoints
-                peakDetectorResults.cumCounts(), l, maxNumKeypoints, 
+    for (size_t l = 0; l < (workings.energyMaps.size() - 1); ++l) {
+        calculator.descriptorExtracter(commandQueue, 
+                workings.dtcwtOut[l], workings.scales[l],      // Subband
+                workings.dtcwtOut[l+1], workings.scales[l+1],  // Parent subband
+                workings.peakDetectorResults.list(),         // Locations of keypoints
+                workings.peakDetectorResults.cumCounts(), l, maxNumKeypoints, 
                         // Start indices within list of the different 
                         // levels; which level to extract; what the maximum
                         // number of keypoints we could be asking for is.
-                descriptors,
-                peakDetectorResults.listDone(),
+                workings.descriptors,
+                workings.peakDetectorResults.listDone(),
                         // The cumulative counts rely on everything else
                         // in the peak detector being done
-                &descriptorsDone_[l], &descriptorsDone_[l + energyMaps.size()]
+                &workings.descriptorsDone[l], &workings.descriptorsDone[l + workings.energyMaps.size()]
                         // Wait for both coarse and fine to be done
                 );
     }
