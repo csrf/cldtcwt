de2504cf52d3ce98ba535d056256a7a6658ea06b
Rich Wareham <rjw57@cam.ac.uk>
2013-07-15 22:06:34 +0100

    remove MATLAB and octave scripts
    The scripts form part of @tegale's infrastructure and won't be used for
    this project. The original, untouched repository is tracked in the
    upstream branch.

--- octave/runDtcwtForVerifyBasic.m
+++
@@ -1,9 +0,0 @@
-
-% Set up the input in the same way as in verifyBasic
-input = zeros(128,128);
-input(64,64) = 1;
-
-[Yl, Yh, Yscale, lohi] = dtwavexfm2b(input, 6, 'near_sym_b_bp', 'qshift_b_bp');
-
-
-
