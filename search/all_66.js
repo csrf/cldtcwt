8d240834d7fa28397479ecaa723a1aa5c2d6dee1
Rich Wareham <rjw57@cam.ac.uk>
2013-07-22 15:12:49 +0000

    Update documentation

--- search/all_66.js
+++ search/all_66.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['filterx',['FilterX',['../classFilterX.html',1,'']]],
   ['filtery',['FilterY',['../classFilterY.html',1,'']]],
-  ['findkeypoints',['findKeypoints',['../classCalculator.html#a0e7bff0b4634b6845b5a3724b5984e91',1,'Calculator']]],
   ['findmax',['FindMax',['../classFindMax.html',1,'']]],
   ['flush',['flush',['../classcl_1_1CommandQueue.html#a4349d9530f98d7544d1ddb5cbcc053bd',1,'cl::CommandQueue']]],
   ['formatcontext',['FormatContext',['../classAV_1_1FormatContext.html',1,'AV']]],
--- search/functions_66.js
+++ search/functions_66.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['findkeypoints',['findKeypoints',['../classCalculator.html#a0e7bff0b4634b6845b5a3724b5984e91',1,'Calculator']]],
   ['flush',['flush',['../classcl_1_1CommandQueue.html#a4349d9530f98d7544d1ddb5cbcc053bd',1,'cl::CommandQueue']]]
 ];
